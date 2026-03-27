54ee06ea3a39b9e2ad693d6a80329af597b9b12b
Alice P. <alicepav231@gmail.com>
2026-03-27 20:58:05 +0100

    Update esp32_led_wifi.ino

--- esp32_led_wifi.ino
+++ esp32_led_wifi.ino
@@ -1,1 +1,53 @@
+#include <WiFi.h>
+#include <WebServer.h>
 
+const char* ssid = "YOUR_SSID";
+const char* password = "YOUR_PASSWORD";
+
+WebServer server(80);
+
+const int ledPin = 2;
+
+void handleRoot() 
+{
+  String html = "<h1>ESP32 LED Control</h1><a href='/on'>ON</a> | <a href='/off'>OFF</a>";
+  server.send(200, "text/html", html);
+}
+
+void handleOn() 
+{
+  digitalWrite(ledPin, HIGH);
+  server.sendHeader("Location","/");
+  server.send(303,"text/plain","");
+}
+
+void handleOff() 
+{
+  digitalWrite(ledPin, LOW);
+  server.sendHeader("Location","/");
+  server.send(303,"text/plain","");
+}
+
+void setup() 
+{
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected: " + WiFi.localIP().toString());
+
+  server.on("/", handleRoot);
+  server.on("/on", handleOn);
+  server.on("/off", handleOff);
+  server.begin();
+}
+
+void loop() 
+{
+  server.handleClient();
+}
