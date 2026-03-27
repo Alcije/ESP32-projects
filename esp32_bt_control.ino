98c5d8e12855e98f3a2a11085981b379ca36f885
Alice P. <alicepav231@gmail.com>
2026-03-27 21:01:34 +0100

    Update esp32_bt_control.ino

--- esp32_bt_control.ino
+++ esp32_bt_control.ino
@@ -1,1 +1,21 @@
+#include "BluetoothSerial.h"
 
+BluetoothSerial SerialBT;
+const int ledPin = 2;
+
+void setup() 
+{
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(115200);
+  SerialBT.begin("ESP32_BT");
+}
+
+void loop() 
+{
+  if (SerialBT.available()) 
+  {
+    char cmd = SerialBT.read();
+    if (cmd == '1') digitalWrite(ledPin, HIGH);
+    if (cmd == '0') digitalWrite(ledPin, LOW);
+  }
+}
