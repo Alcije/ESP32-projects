adac3097fe151fca6a3b899a4278db8df71a63ab
Alice P. <alicepav231@gmail.com>
2026-03-27 20:58:58 +0100

    Update esp32_temp_web.ino

--- esp32_temp_web.ino
+++ esp32_temp_web.ino
@@ -1,1 +1,39 @@
+#include <WiFi.h>
+#include <WebServer.h>
+#include "DHT.h"
 
+#define DHTPIN 4
+#define DHTTYPE DHT11
+
+const char* ssid = "YOUR_SSID";
+const char* password = "YOUR_PASSWORD";
+
+DHT dht(DHTPIN, DHTTYPE);
+WebServer server(80);
+
+void handleRoot() 
+{
+  float temp = dht.readTemperature();
+  String html = "<h1>ESP32 Temperature</h1><p>Temp: " + String(temp) + " °C</p>";
+  server.send(200, "text/html", html);
+}
+
+void setup() 
+{
+  Serial.begin(115200);
+  dht.begin();
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) 
+  { 
+    delay(500); Serial.print("."); 
+  }
+  Serial.println("WiFi connected: " + WiFi.localIP().toString());
+
+  server.on("/", handleRoot);
+  server.begin();
+}
+
+void loop() 
+{
+  server.handleClient();
+}
