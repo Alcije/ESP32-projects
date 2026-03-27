620ed53e1861f46683fba7cbaedda220834cdd0a
Alice P. <alicepav231@gmail.com>
2026-03-27 21:02:58 +0100

    Update esp32_home.ino

--- esp32_home.ino
+++ esp32_home.ino
@@ -1,1 +1,56 @@
+#include <WiFi.h>
+#include <WebServer.h>
+#include "DHT.h"
 
+#define DHTPIN 4
+#define DHTTYPE DHT11
+#define LEDPIN 2
+
+const char* ssid = "YOUR_SSID";
+const char* password = "YOUR_PASSWORD";
+
+DHT dht(DHTPIN, DHTTYPE);
+WebServer server(80);
+
+void handleRoot() 
+{
+  float temp = dht.readTemperature();
+  float hum = dht.readHumidity();
+  String html = "<h1>ESP32 Home Control</h1>";
+  html += "<p>Temperature: " + String(temp) + " °C</p>";
+  html += "<p>Humidity: " + String(hum) + " %</p>";
+  html += "<p>LED: <a href='/on'>ON</a> | <a href='/off'>OFF</a></p>";
+  server.send(200, "text/html", html);
+}
+
+void handleOn() 
+{ 
+  digitalWrite(LEDPIN,HIGH); server.sendHeader("Location","/"); server.send(303,"text/plain",""); 
+}
+void handleOff() 
+{ 
+  digitalWrite(LEDPIN,LOW); server.sendHeader("Location","/"); server.send(303,"text/plain",""); 
+}
+
+void setup() 
+{
+  pinMode(LEDPIN, OUTPUT);
+  Serial.begin(115200);
+  dht.begin();
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) 
+  { 
+    delay(500); Serial.print("."); `
+  }
+  Serial.println("WiFi connected: " + WiFi.localIP().toString());
+
+  server.on("/", handleRoot);
+  server.on("/on", handleOn);
+  server.on("/off", handleOff);
+  server.begin();
+}
+
+void loop() 
+{
+  server.handleClient();
+}
