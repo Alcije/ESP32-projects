3e6e41655141958a1dd181b125db244d5ae4fc86
Alice P. <alicepav231@gmail.com>
2026-03-27 21:30:11 +0100

    Update esp32_light_web.ino

--- esp32_light_web.ino
+++ esp32_light_web.ino
@@ -1,1 +1,35 @@
+#include <WiFi.h>
+#include <WebServer.h>
 
+const char* ssid = "YOUR_SSID";
+const char* password = "YOUR_PASSWORD";
+
+const int ldrPin = 34;
+WebServer server(80);
+
+void handleRoot() 
+{
+  int ldrValue = analogRead(ldrPin);
+  String html = "<h1>ESP32 Light Sensor</h1>";
+  html += "<p>Light Level: " + String(ldrValue) + "</p>";
+  server.send(200, "text/html", html);
+}
+
+void setup() 
+{
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while(WiFi.status() != WL_CONNECTED) 
+  { 
+    delay(500); Serial.print(".");
+  }
+  Serial.println("WiFi connected: " + WiFi.localIP().toString());
+  
+  server.on("/", handleRoot);
+  server.begin();
+}
+
+void loop() 
+{
+  server.handleClient();
+}
